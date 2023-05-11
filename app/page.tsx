5241abc7fe8b5feec7875ac90c76b230955f4c3f
liurongqing <liurongqing@liurongqingdeiMac.local>
2023-05-12 01:29:50 +0800

    1

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import a from "@alilc/lowcode-engine";
+// import { common, plugins, config } from "@alilc/lowcode-engine";
+
+export default function Home() {
+  // return <div>123</div>;
+  console.log("aa", a);
+  // console.log("common", { config });
+  // const Workbench = common.skeletonCabin.Workbench;
+
+  // return <Workbench />;
+  return 123;
+}
